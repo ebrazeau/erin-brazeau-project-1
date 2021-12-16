0a73e58c7fb0a1e00a014035d8e98819c96a100a
Erin <erin.braz@gmail.com>
2021-12-15 20:20:16 -0500

    fixing up my mobile menu

--- script.js
+++ script.js
@@ -5,17 +5,29 @@
 // 2. on mouse click, reveal the vertical menu
 // 3. click again to hide the menu (Toggle class)
 
-console.log('The script is attached');
 
 // get the icon element
 const hamIcon = document.querySelector('.fa-bars');
+
 // get the ul containing the nav
 const navUl = document.querySelector('.hor-nav');
+
+// Add an event listener that removes our vertical menu class if the screen gets larger than 780px.
+
+var mediaQueryList = window.matchMedia('(min-width: 780px)');
+
+function screenTest(e) {
+    if (e.matches) {
+        navUl.classList.remove('vert-nav');
+    } 
+};
+        
+mediaQueryList.addListener(screenTest);
+        
 
 // add an event listener on the icon
 hamIcon.addEventListener('click', function (event) {
     navUl.classList.toggle('vert-nav');
-});
+});    
 
-// need to figure out how to remove the vert-nav class at the 780px breakpoint. 
-// and apply more styles to the menu
+
